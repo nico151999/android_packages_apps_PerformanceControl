2ea361960ace04101dbdf0158294f3009a36c936
daniel_hk <daniel.p6800@gmail.com>
2016-09-27 00:31:52 +0800

    Version 2.1.6
    1. Revert from system UID
    2. use toybox if busybox not exist

--- Android.mk
+++ Android.mk
@@ -8,7 +8,7 @@ LOCAL_SRC_FILES := \
         $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := PerformanceControl
-LOCAL_CERTIFICATE := platform
+#LOCAL_CERTIFICATE := platform
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v13
 
